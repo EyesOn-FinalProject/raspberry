63aacca2f241268c0afb0afc76b6273859306453
cano721 <cano721@naver.com>
2021-05-12 10:31:12 +0900

    Update camera

---
+++ Camera.py
@@ -0,0 +1,37 @@
+import picamera
+import time
+import io
+import threading
+
+# Camera 클래스는 비디오 스트리밍 - 하나의 프로세스 안에서 독립적인 실행흐름으로 처리하기 위해 쓰레드로 처리
+class Camera:
+    thread = None
+    frame = None
+    start_time = 0
+
+    # streaming이라는 함수를 쓰레드로 관리하고 화면으로 보내주는 메소드
+    def getStreaming(self):
+        Camera.start_time = time.time()
+        if Camera.thread is None:
+            # 백 그라운드의 쓰레드를 시작 - 쓰레드로 작업하기 위해 Thread클래스를 생성해서 작업
+            # ==> 클래스를 만들때 Thread클래스를 상속받아 만들 수 있다.
+            # streaming메소드의 실행을 쓰레드로 처리하겠다는 의미
+            Camera.thread = threading.Thread(target=self.capture)
+            Camera.thread.start() # 쓰레드를 시작하겠다는 의미
+            while self.frame is None:
+                time.sleep(1)
+        return self.frame
+
+    # 독립적인 실행의 한 단위로 파이카메라로 찍은 영상을 프레임단위로 지속적으로 보내주는 역할을 하는 메소드
+    @classmethod
+    def capture(c):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (320,240) # 해상도
+            camera.hflip = True #뒤집는거
+            camera.vflip = True
+
+            for i in range(5):
+                time.sleep(5)
+                camera.capture('/home/pi/iot/image%s.jpg' % i)
+
+            camera.stop_preview()  # 미리보기 화면 정지
